d7a8c6d636d03d111b816c39b4a6c32e9d777d08
Christian Zhirzhan <34308679+damian1998@users.noreply.github.com>
2022-10-12 00:05:08 -0500

    update  experience

--- js/portafolio.js
+++ js/portafolio.js
@@ -19,26 +19,6 @@ var portfolioData = [
     video: 'https://youtube.com/shorts/NPh3SKf0OWA',
     buttons: [false, false, false, 'http://artacademy.nintendo.com/disney/'],
   },
-  {
-    title:
-      'Aplicación Móvil para el Agendamiento de Citas de Consultas Médicas Utilizando Técnicas de Procesamiento de Lenguaje Natural',
-    subtitle: "Promo website for Nintendo's 3DS game",
-    description:
-      'Fue desarrollado usando tecnologías como Spring, PostgreSQL, AWS, Ionic y Rasa Framework, se implemento la autenticación de los usuarios por tokens para acceder a servicios de escritura y lectura privados, para la persistencia en la base de datos se usó Spring Data JPA.',
-    code: 'disney',
-    video: 'https://youtube.com/shorts/NPh3SKf0OWA',
-    buttons: [false, false, false, 'http://artacademy.nintendo.com/disney/'],
-  },
-  {
-    title:
-      'Aplicación Móvil para el Agendamiento de Citas de Consultas Médicas Utilizando Técnicas de Procesamiento de Lenguaje Natural',
-    subtitle: "Promo website for Nintendo's 3DS game",
-    description:
-      'Fue desarrollado usando tecnologías como Spring, PostgreSQL, AWS, Ionic y Rasa Framework, se implemento la autenticación de los usuarios por tokens para acceder a servicios de escritura y lectura privados, para la persistencia en la base de datos se usó Spring Data JPA.',
-    code: 'disney',
-    video: 'https://youtube.com/shorts/NPh3SKf0OWA',
-    buttons: [false, false, false, 'http://artacademy.nintendo.com/disney/'],
-  },
 ];
 
 /* <iframe  src="https://www.youtube.com/embed/NPh3SKf0OWA" frameborder="0" allowfullscreen></iframe> */
